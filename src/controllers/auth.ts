f88348600c86b98f155669ec1a76ff73d0781521
HyoketsuSenpai <Youseftela@gmail.com>
2026-01-07 09:02:38 +0300

    refactor: add error handling to auth controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,113 +1,131 @@
 import type {Request, Response} from 'express';
 import db from '../config/db/prisma_db.js';
 import bcrypt from "bcrypt";
-import jwt, {type JwtPayload}from 'jsonwebtoken';
+import jwt, {JsonWebTokenError, TokenExpiredError, type JwtPayload}from 'jsonwebtoken';
 
 import setRefreshToken from '../utils/set_refresh_token.js';
 import getRefreshToken from '../utils/get_refresh_token.js';
 import deleteRefreshToken from '../utils/delete_refresh_token.js';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 async function signUp(req: Request, res: Response){
-    const {name, email, password, confirmPassword} = req.body;
+    const {name, email, password} = req.body;
 
-    // sanitize that shi before anything bruh
+    // add try catches everywhere
+    try {
 
-    const exist = await db.user.findUnique({
-        where: {email},
-    });
-
-    if(exist){
-        return res.status(400).json({message:"Email already used"});
-    }
-
-    // hash the password before saving it
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-    const user = await db.user.create({
-        data: {
-            name,
-            email,
-            password: hashedPassword,
+        const exist = await db.user.findUnique({
+            where: {email},
+        });
+        
+        if(exist){
+            return res.status(400).json({message:"Email already used"});
         }
-    });
 
-    const accessToken = jwt.sign(
-        {
-            id: user.id,
-            email: user.email,
-            name: user.name ?? "Nameless Bum",
-        },
-        process.env.ACCESS_TOKEN_SECRET!,
-        {expiresIn: '30s'}
-    );
+        // hash the password before saving it
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        
+        const user = await db.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword,
+            }
+        });
 
-    const refreshToken = jwt.sign(
-        {
-            id: user.id,
-            email: user.email,
-            name: user.name ?? "Nameless Bum",
-        },
-        process.env.REFRESH_TOKEN_SECRET!,
-    )
+        const accessToken = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+                name: user.name ?? "Nameless Bum",
+            },
+            process.env.ACCESS_TOKEN_SECRET!,
+            {expiresIn: '30m'}
+        );
 
-    // add refresh token to db
-    await setRefreshToken(refreshToken);
+        const refreshToken = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+                name: user.name ?? "Nameless Bum",
+            },
+            process.env.REFRESH_TOKEN_SECRET!,
+        )
 
-    return res.status(201).json({
-        message: "User Created Sucessfully",
-        accessToken,
-        refreshToken
-    });
+        // add refresh token to db
+        await setRefreshToken(refreshToken);
+        
+        return res.status(201).json({
+            message: "User Created Sucessfully",
+            accessToken,
+            refreshToken
+        });
 
+    } catch(e){
+        console.error(e);
+
+        if (e instanceof PrismaClientKnownRequestError && e.code === 'P2002') {
+        return res.status(400).json({ message: "Email already used" });
+        }
+
+        return res.sendStatus(500);
+    }
 }
 
 async function signIn(req: Request, res: Response){
     const {email, password} = req.body;
 
-    const user = await db.user.findUnique({
-        where: {
-            email
+    try {
+        
+        const user = await db.user.findUnique({
+            where: {
+                email
+            }
+        });
+        
+        if(!user){
+            return res.status(401).json({message:'Wrong Credentials'});
         }
-    });
+        
+        const match: boolean = await bcrypt.compare(password, user.password);
+        
+        if(!match){
+            return res.status(401).json({message:'Wrong Credentials'});
+        }
 
-    if(!user){
-        return res.status(401).json({message:'Wrong Credentials'});
+        const accessToken = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+                name: user.name ?? "Nameless Bum",        
+            },
+            process.env.ACCESS_TOKEN_SECRET!, 
+            {expiresIn: '30m'}
+        );
+
+        const refreshToken = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+                name: user.name ?? "Nameless Bum",
+            },
+            process.env.REFRESH_TOKEN_SECRET!,
+        )
+
+        // add refresh token to db
+        await setRefreshToken(refreshToken);
+        
+        return res.status(200).json({
+            message:'Logged in successfully',
+            accessToken,
+            refreshToken
+        });
+
+    } catch(e) {
+        console.error(e);
+        res.sendStatus(500);
     }
-
-    const match: boolean = await bcrypt.compare(password, user.password);
-
-    if(!match){
-        return res.status(401).json({message:'Wrong Credentials'});
-    }
-
-    const accessToken = jwt.sign(
-        {
-            id: user.id,
-            email,
-            name: user.name ?? "Nameless Bum",        
-        },
-        process.env.ACCESS_TOKEN_SECRET!, 
-        {expiresIn: '30s'}
-    );
-
-    const refreshToken = jwt.sign(
-        {
-            id: user.id,
-            email: user.email,
-            name: user.name ?? "Nameless Bum",
-        },
-        process.env.REFRESH_TOKEN_SECRET!,
-    )
-
-    // add refresh token to db
-    await setRefreshToken(refreshToken);
-
-    res.status(200).json({
-        message:'Logged in successfully',
-        accessToken,
-        refreshToken
-    });
 
 }
 
@@ -115,36 +133,54 @@ async function token(req: Request, res: Response){
     const refreshToken = req.body.token;
 
     // if (refreshToken == null) return res.sendStatus(401);
-    //check for refresh token in db
-    const exists = await getRefreshToken(refreshToken);
 
-    if(!exists){
-        return res.sendStatus(401);
+    try {
+
+        //check for refresh token in db
+        const exists = await getRefreshToken(refreshToken);
+
+        if(!exists){
+            return res.sendStatus(401);
+        }
+
+        const user = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET!);
+        if (typeof user === 'string') {
+            return res.sendStatus(403);
+        }
+
+        const accessToken = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+                name: user.name ?? "Nameless Bum",
+            },
+            process.env.ACCESS_TOKEN_SECRET!,
+            {expiresIn:'30m'}
+        );
+
+        return res.json({accessToken});
+    } catch(e) {
+        console.error(e);
+
+        if(e instanceof JsonWebTokenError) return res.sendStatus(403);
+
+        if(e instanceof TokenExpiredError) return res.sendStatus(401);
+
+        res.sendStatus(500);
     }
 
-    const user = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET!);
-    if (typeof user === 'string') {
-        return res.sendStatus(403);
-    }
-
-    const accessToken = jwt.sign(
-        {
-            id: user.id,
-            email: user.email,
-            name: user.name ?? "Nameless Bum",
-        },
-        process.env.ACCESS_TOKEN_SECRET!,
-        {expiresIn:'30s'}
-    );
-
-    return res.json({accessToken});
 }
 
 async function signOut(req: Request, res: Response){
     const refreshToken = req.body.token;
-    // delete token from db
-    await deleteRefreshToken(refreshToken);
-    res.sendStatus(203);
+    try {
+        // delete token from db
+        await deleteRefreshToken(refreshToken);
+        res.sendStatus(204);
+    } catch(e) {
+        console.error(e);
+        res.sendStatus(500);
+    }
 }
 
 export {signUp, signIn, token, signOut}
--- src/utils/set_refresh_token.ts
+++ src/utils/set_refresh_token.ts
@@ -2,15 +2,16 @@ import redis from './../config/db/redis_db.js';
 import crypto from 'crypto';
 
 async function setRefreshToken(refreshToken: string){
-    const hashedToken = crypto.createHash('sha256').update(refreshToken).digest('hex');
 
-    await redis.set(`refresh:${hashedToken}`, refreshToken, {
+    const hashedToken = crypto.createHash('sha256').update(refreshToken).digest('hex');
+    
+    await redis.set(`refresh:${hashedToken}`, hashedToken, {
         expiration: {
             type: 'EX',
             value: 60 * 60 * 24 * 60
         }
     });
-
+            
 }
 
 export default setRefreshToken
